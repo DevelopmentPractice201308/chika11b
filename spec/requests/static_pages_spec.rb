861a8564bd765ac9dd4e80f32f833077737c2460
Chika Sesoko <chika_s-11b@stu.kbc.ac.jp>
2013-09-17 11:25:01 +0900

    Finish user signup

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -5,7 +5,7 @@ describe "Static pages" do
 
   subject { page }
 
-   shared_examples_for "all static pages" do
+  shared_examples_for "all static pages" do
     it { should have_selector('h1',    text: heading) }
     it { should have_selector('title', text: full_title(page_title)) }
   end 
@@ -35,21 +35,8 @@ describe "Static pages" do
   describe "Contact page" do
     before { visit contact_path }
 
-    it { should have_selector('h1',    text: 'Contact') }
-    it { should have_selector('title', text: full_title('Contact')) }
-  end
-  it "should have the right links on the layout" do
-    visit root_path
-    click_link "About"
-    page.should have_selector 'title', text: full_title('About Us')
-    click_link "Help"
-    page.should # fill in
-    click_link "Contact"
-    page.should # fill in
-    click_link "Home"
-    click_link "Sign up now!"
-    page.should # fill in
-    click_link "sample app"
-    page.should # fill in
-  end
+      it { should have_selector('h1',    text: 'Contact') }
+      it { should have_selector('title', text: full_title('Contact')) }
+    end
+
 end
